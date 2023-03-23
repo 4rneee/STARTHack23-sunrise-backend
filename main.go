78e54ed6fe044e81c5f8fa941f7647e1946ad80c
nschlenker <noah.schlenker2002@gmail.com>
2023-03-23 02:02:07 +0100

    main gin

--- main.go
+++ main.go
@@ -8,5 +8,10 @@ import (
 func main() {
     router := gin.Default()
     router.PUT("addUser/", controllers.AddNewUser) 
+    router.GET("login/", controllers.LoginUser)
+
     router.PUT("addFriendship/", controllers.AddNewFriendship)
+    router.GET("getFriends/", controllers.GetActiveFriends)
+
+    router.Run("localhost:8080")
 }
